1d5013b01110bf2bb4cdeb701028c0a22a6e5e2d
socsel-TT <s2310157@wakayama-u.ac.jp>
2025-06-18 08:31:32 +0000

    パーティのステータスの表示形式の改善

--- src/rpg/character/hero/HeroParty.java
+++ src/rpg/character/hero/HeroParty.java
@@ -20,7 +20,7 @@ public class HeroParty extends AbstractParty{
 		for(AbstractCharacter member: super.getMembers()) {
 			Hero hero = (Hero) member;
 			if(hero.getHp() > 0  && !hero.isEscaped()) {
-				System.out.print(hero.getName() + ")" + hero.getJob() + "(;" + hero.getHp() + " ");
+				System.out.print(hero.getName() + "(職業:" + hero.getJob() + " HP:" + hero.getHp() + ") ");
 			}else if(member.isEscaped()) {
 				System.out.println(hero.getName() + "(" + hero.getJob() + "):死亡 ");
 			}else if(member.getHp() <= 0) {
