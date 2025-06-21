9cdccd8aae6e1c6c97a94fa60b6b38ca6cfa9d07
s2310176 <s2310176@wakayama-u.ac.jp>
2025-06-22 00:48:10 +0900

    「死亡」と「逃亡」の表記の修正

--- src/rpg/character/hero/HeroParty.java
+++ src/rpg/character/hero/HeroParty.java
@@ -22,9 +22,9 @@ public class HeroParty extends AbstractParty{
 			if(hero.getHp() > 0  && !hero.isEscaped()) {
 				System.out.print(hero.getName() + ")" + hero.getJob() + "(;" + hero.getHp() + " ");
 			}else if(member.isEscaped()) {
-				System.out.println(hero.getName() + "(" + hero.getJob() + "):死亡 ");
+				System.out.println(hero.getName() + "(" + hero.getJob() + "):逃亡 ");// 死亡→逃亡に修正
 			}else if(member.getHp() <= 0) {
-				System.out.println(hero.getName() + "(" + hero.getJob() + "):逃亡 ");
+				System.out.println(hero.getName() + "(" + hero.getJob() + "):死亡 ");// 逃亡→死亡に修正
 			}
 		}
 		System.out.println();  //レイアウト調整のための改行
